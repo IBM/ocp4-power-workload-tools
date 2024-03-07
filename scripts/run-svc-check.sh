702c41f807e58e9ef075a1b8f6d22715cd899e05
Paul Bastide <pbastide@us.ibm.com>
2024-03-07 15:43:24 -0500

    OCTOPUS-596: refactor for running from main directory
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- scripts/run-svc-check.sh
+++ scripts/run-svc-check.sh
@@ -2,18 +2,19 @@
 
 # The default port range is 30000-32767
 # https://docs.openshift.com/container-platform/4.15/networking/configuring-node-port-service-range.html
-for PORT in echo {30000..32767}
+for PORT in {30000..32767}
 do
-    echo "\nTESTING PORT: ${PORT}"
-    cat pod-service.yaml | sed 's|30007|'${PORT}'|g' | oc apply -f -
-    echo "waiting 10 seconds"
-    sleep 10
+    echo "\nTESTING PORT: ${PORT}\n"
+    cat manifests/network/pod-service.yaml | sed "s|30007|${PORT}|g" \
+        | oc apply -f -
+    echo "\nwaiting 3 seconds\n"
+    sleep 3
 
     for TRY in echo {0..5}
     do
         echo "TRY: ${TRY}"
         echo "Putting a debug pod on the opposite - powerworker"
-        oc apply -f pod-debug-run.yaml
+        oc apply -f manifests/network/pod-debug-run.yaml
         sleep 1
 
         oc rsh pod/runner curl http://power-net-workload-svc.openshift-net-workload.svc.cluster.local:8080/status -k
