971e486641ca5424a59d23a8c21a0de46d3db26a
Paul Bastide <pbastide@us.ibm.com>
2024-03-11 12:45:51 -0400

    OCTOPUS-596: modify for simplified logs
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- scripts/run-svc-check.sh
+++ scripts/run-svc-check.sh
@@ -2,24 +2,54 @@
 
 oc apply -f manifests/network/ds-debug-run.yaml
 
-# The default port range is 30000-32767
-# https://docs.openshift.com/container-platform/4.15/networking/configuring-node-port-service-range.html
-for PORT in {30000..32767}
-do
-    echo "\nTESTING PORT: ${PORT}\n"
-    cat manifests/network/pod-service.yaml | sed "s|30007|${PORT}|g" \
-        | oc apply -f -
-    echo "\nwaiting 3 seconds\n"
-    sleep 3
-
-    for TRY in echo {0..100}
+if [ ! -f manifests/network/pod-service.yaml ]
+then
+    echo "file doesn't exist: manifests/network/pod-service.yaml"
+    echo "check running scripts/run-svc-check.sh"
+    exit 1
+else
+    # The default port range is 30000-32767
+    # https://docs.openshift.com/container-platform/4.15/networking/configuring-node-port-service-range.html
+    for PORT in {30000..32767}
     do
-        echo "TRY: ${TRY}"
-        echo "Putting a debug pod on the opposite - powerworker"
+        echo "TESTING PORT: ${PORT}"
+        cat manifests/network/pod-service.yaml | sed "s|30007|${PORT}|g" \
+            | oc apply -f -
+        echo "waiting 1 seconds"
         sleep 1
 
-        # fail in logs is ok.
-        oc rsh daemonset/debug-runner curl http://power-net-workload-svc.openshift-net-workload.svc.cluster.local:8080/status -k -v
-        [ $? -eq 1 ] && echo "Failed to curl"
+        for TRY in {1..10}
+        do
+            echo ""
+            echo "TRY: ${TRY}"
+            echo "Putting a debug pod on the opposite - powerworker"
+
+            # fail in logs is ok.
+            oc rsh daemonset/debug-runner curl http://power-net-workload-svc.openshift-net-workload.svc.cluster.local:8080/status -k -o /dev/null -w "%{http_code}" -s \
+                || echo "Failed to curl" &
+        done
+        wait $(jobs -p)
+        for TRY in {11..20}
+        do
+            echo ""
+            echo "TRY: ${TRY}"
+            echo "Putting a debug pod on the opposite - powerworker"
+
+            # fail in logs is ok.
+            oc rsh daemonset/debug-runner curl http://power-net-workload-svc.openshift-net-workload.svc.cluster.local:8080/status -k -o /dev/null -w "%{http_code}" -s \
+                || echo "Failed to curl" &
+        done
+        wait $(jobs -p)
+        for TRY in {21..30}
+        do
+            echo ""
+            echo "TRY: ${TRY}"
+            echo "Putting a debug pod on the opposite - powerworker"
+
+            # fail in logs is ok.
+            oc rsh daemonset/debug-runner curl http://power-net-workload-svc.openshift-net-workload.svc.cluster.local:8080/status -k -o /dev/null -w "%{http_code}" -s \
+                || echo "Failed to curl" &
+        done
+        wait $(jobs -p)
     done
-done
+fi
