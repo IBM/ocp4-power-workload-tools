c7ab707b2215df19bb0de6bde72560c106aa5180
Paul Bastide <pbastide@us.ibm.com>
2024-10-02 15:37:43 -0400

    fix: nginx temp folder needs to be set
    Signed-off-by: Paul Bastide <pbastide@us.ibm.com>

--- scripts/nx-gzip/00-prep.sh
+++ scripts/nx-gzip/00-prep.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 oc label node/worker-0 cpu-coprocessor.nx_gzip=true
+oc create imagestream nx-gzip
+
+if [ -n "${NON_PROD}" ]
+then
+echo "Setting non-prod status for imageregistry"
+oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"storage":{"emptyDir":{}}}}'
+fi
